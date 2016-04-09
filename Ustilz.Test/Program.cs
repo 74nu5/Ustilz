0440b06c3009ec3a23230fe74fee20b249379ed3
Romain AVONDE <romain.avonde@capgemini.com>
2016-04-09 15:01:47 +0200

    Ajout du projet de test

---
+++ Ustilz.Test/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ustilz.Test
+{
+    #region Usings
+
+    using System;
+    using System.Collections.Generic;
+
+    using Ustilz.Extensions;
+
+    #endregion
+
+    /// <summary>The program.</summary>
+    internal class Program
+    {
+        /// <summary>The main.</summary>
+        /// <param name="args">The args.</param>
+        private static void Main(string[] args)
+        {
+            string[] ss = { "1", "3", "TT" };
+            ss.DumpConsole();
+
+            List<Version> vv = new List<Version>() { new Version("8.8.9.8"), new Version("9.0.7.6") };
+            vv.DumpConsole();
+        }
+    }
+}
