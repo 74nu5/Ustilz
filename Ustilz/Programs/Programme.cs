dacd77018db3c204dd0d473a66177f1d4f7324f0
Romain AVONDE <romain.avonde@capgemini.com>
2016-04-09 17:49:19 +0200

    Ajout de l'aide au programme

---
+++ Ustilz/Programs/Programme.cs
@@ -0,0 +1,65 @@
+﻿namespace Ustilz.Programs
+{
+    #region Usings
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Ustilz.Annotations;
+
+    #endregion
+
+    /// <summary>The programme.</summary>
+    [PublicAPI]
+    public static class Programme
+    {
+        #region Méthodes statiques
+
+        /// <summary>The with choice.</summary>
+        /// <param name="actions">The actions.</param>
+        public static void WithChoice(params Action[] actions)
+        {
+            WithChoice(false, actions);
+        }
+
+        /// <summary>The with choice.</summary>
+        /// <param name="withExit">The with exit.</param>
+        /// <param name="actions">The actions.</param>
+        public static void WithChoice(bool withExit, params Action[] actions)
+        {
+            IList<Action> enumerable = actions.ToList();
+            do
+            {
+                int i = 1;
+                foreach (Action action in actions)
+                {
+                    Console.WriteLine("{0}) {1}", i++, action.Method.Name);
+                }
+
+                if (withExit)
+                {
+                    Console.WriteLine("{0}) Exit", i);
+                    enumerable.Add(() => Environment.Exit(-1));
+                }
+
+                string entry = Console.ReadLine();
+                if (enumerable.Count == 0)
+                {
+                    return;
+                }
+
+                int choix;
+                bool isChoixNumerique = int.TryParse(entry, out choix);
+
+                if (!isChoixNumerique || (choix <= enumerable.Count && choix > 0))
+                {
+                    enumerable[choix - 1].Invoke();
+                }
+            }
+            while (true);
+        }
+
+        #endregion
+    }
+}
