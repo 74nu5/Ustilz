4ddaf852afdbd0e67d555924bfb5c7a53a9c907d
Tanus <romain@avonde.net>
2018-09-16 17:54:59 +0200

    Corrections des warnings dans le projet Ustilz.Xml

--- Ustilz.Xml/XmlExtensions.cs
+++ Ustilz.Xml/XmlExtensions.cs
@@ -1,7 +1,10 @@
-﻿namespace Ustilz.Xml
+namespace Ustilz.Xml
 {
     #region Usings
 
+    using System;
+    using System.IO;
+    using System.Xml;
     using System.Xml.Linq;
     using System.Xml.Serialization;
 
@@ -15,23 +18,49 @@
     {
         #region Méthodes publiques
 
-        /// <summary>The deserialize.</summary>
-        /// <param name="xmlDocument">The xml document.</param>
-        /// <typeparam name="T">Le type vers lequel désérialiser</typeparam>
-        /// <returns>The <see cref="T" />.</returns>
-        public static T FromXml<T>([NotNull] this XDocument xmlDocument)
+        /// <summary>Méthode de dé-sérialisation à partir d'un XDocument.</summary>
+        /// <param name="xDocument">Le XDocument à dé-sérialiser.</param>
+        /// <typeparam name="T">Le type vers lequel dé-sérialiser.</typeparam>
+        /// <returns>Retourne l'objet de T dé-sérialisé.</returns>
+        public static T FromXml<T>([NotNull] this XDocument xDocument)
         {
             var xmlSerializer = new XmlSerializer(typeof(T));
-            using (var reader = xmlDocument.CreateReader())
+            using (var reader = xDocument.CreateReader())
             {
                 return (T)xmlSerializer.Deserialize(reader);
             }
         }
 
-        /// <summary>The serialize to xml.</summary>
-        /// <param name="obj">The obj.</param>
-        /// <typeparam name="T">Type a sérialiser</typeparam>
-        /// <returns>The <see cref="string" />.</returns>
+        /// <summary>Méthode de dé-sérialisation à partir d'une chaine de caractère.</summary>
+        /// <param name="xmlStr">La chaine de caractères à dé-sérialiser.</param>
+        /// <typeparam name="T">Le type vers lequel dé-sérialiser.</typeparam>
+        /// <returns>Retourne l'objet de T dé-sérialisé.</returns>
+        public static T FromXml<T>([NotNull] this string xmlStr)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(T));
+            using (var reader = new StringReader(xmlStr ?? throw new NullReferenceException("La chaine de caractères ne peut pas être nulle.")))
+            {
+                return (T)xmlSerializer.Deserialize(reader);
+            }
+        }
+
+        /// <summary>Méthode de dé-sérialisation à partir d'un document Xml.</summary>
+        /// <param name="xmlDocument">Document Xml à dé-sérialiser.</param>
+        /// <typeparam name="T">Le type vers lequel dé-sérialiser.</typeparam>
+        /// <returns>Retourne l'objet de T dé-sérialisé.</returns>
+        public static T FromXml<T>([NotNull] this XmlDocument xmlDocument)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(T));
+            using (var reader = new XmlNodeReader(xmlDocument.DocumentElement ?? throw new InvalidOperationException("Le document ne peut pas être null.")))
+            {
+                return (T)xmlSerializer.Deserialize(reader);
+            }
+        }
+
+        /// <summary>Méthode de sérialisation d'un objet.</summary>
+        /// <param name="obj">L'objet à sérialiser.</param>
+        /// <typeparam name="T">Type a sérialiser.</typeparam>
+        /// <returns>Retourne une chaine de caractères représentant l'objet sous format Xml.</returns>
         public static string ToXml<T>(this T obj)
         {
             var doc = new XDocument();
