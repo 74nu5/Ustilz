ec8d6b290414252897aed3c4ae764feaeb29b256
Tanus <romain@avonde.net>
2018-09-16 17:55:06 +0200

    Corrections des warnings dans le projet Ustilz.Json

--- Ustilz.Json/JsonExtensions.cs
+++ Ustilz.Json/JsonExtensions.cs
@@ -1,4 +1,4 @@
-﻿namespace Ustilz.Json
+namespace Ustilz.Json
 {
     #region Usings
 
@@ -7,23 +7,23 @@
     using Newtonsoft.Json;
 
     #endregion
-    
+
     /// <summary>The json extensions.</summary>
     [PublicAPI]
     public static class JsonExtensions
     {
         #region Méthodes publiques
 
-        /// <summary>The deserialize.</summary>
-        /// <param name="json">The json.</param>
-        /// <typeparam name="T">Type à désérialiser</typeparam>
-        /// <returns>The <see cref="T" />.</returns>
+        /// <summary>Méthode de dé-sérialisation à partir d'une chaine de caractères.</summary>
+        /// <param name="json">La chaine de caractères représentant le json à dé-sérialiser.</param>
+        /// <typeparam name="T">Type à dé-sérialiser.</typeparam>
+        /// <returns>L'objet de type T.</returns>
         public static T FromJson<T>(this string json) => JsonConvert.DeserializeObject<T>(json);
 
-        /// <summary>The serialize.</summary>
-        /// <param name="obj">The obj.</param>
-        /// <typeparam name="T">Type à partir duquel sérialiser</typeparam>
-        /// <returns>The <see cref="string" />.</returns>
+        /// <summary>Méthode de sérialisation d'un objet en Json.</summary>
+        /// <param name="obj">L'objet à sérialiser.</param>
+        /// <typeparam name="T">Type à partir duquel sérialiser.</typeparam>
+        /// <returns>Retourne une chaine de caractères représentant l'objet en paramètre.</returns>
         public static string ToJson<T>([NotNull] this T obj) => JsonConvert.SerializeObject(obj, obj.GetType(), Formatting.Indented, null);
 
         #endregion
