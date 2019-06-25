0c2234d25a7c0fb3a0062155550e5dcee6115ed4
Tanus <romain@avonde.net>
2019-06-25 20:23:40 +0200

    [Data] Warnings corrections

--- src/Ustilz.Data/BaseDAL.cs
+++ src/Ustilz.Data/BaseDAL.cs
@@ -39,9 +39,10 @@ namespace Ustilz.Data
 
         /// <summary>Initialise une nouvelle instance de la classe <see cref="BaseDAL{Tcontext, TModel, TIdentity}" />.</summary>
         /// <param name="context">The context.</param>
-        protected BaseDAL(TContext context)
+        /// <exception cref="ArgumentNullException"><paramref name="context" /> is null.</exception>
+        protected BaseDAL([NotNull] TContext context)
         {
-            this.context = context;
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
             this.Queryable = this.context.Set<TModel>().AsQueryable();
         }
 
@@ -57,51 +58,32 @@ namespace Ustilz.Data
         #region Méthodes publiques
 
         /// <inheritdoc />
-        public int Add(TModel model)
-        {
-            this.context.Set<TModel>().Add(model);
-            return this.context.SaveChanges();
-        }
-
-        /// <inheritdoc />
+        /// <exception cref="DbUpdateException">An error is encountered while saving to the database.</exception>
+        /// <exception cref="DbUpdateConcurrencyException">
+        ///     A concurrency violation is encountered while saving to the database. A concurrency violation occurs when an unexpected number of
+        ///     rows are affected during save. This is usually because the data in the database has been modified since it was loaded into memory.
+        /// </exception>
         public async Task<int> AddAsync(TModel model, CancellationToken stoppingToken = default)
         {
-            await this.context.Set<TModel>().AddAsync(model, stoppingToken);
-            return await this.context.SaveChangesAsync(stoppingToken);
+            await this.context.Set<TModel>().AddAsync(model, stoppingToken).ConfigureAwait(false);
+            return await this.context.SaveChangesAsync(stoppingToken).ConfigureAwait(false);
         }
 
         /// <inheritdoc />
-        public int AddRange(IEnumerable<TModel> models)
-        {
-            this.context.Set<TModel>().AddRange(models);
-            return this.context.SaveChanges();
-        }
-
-        /// <inheritdoc />
+        /// <exception cref="DbUpdateException">An error is encountered while saving to the database.</exception>
+        /// <exception cref="DbUpdateConcurrencyException">
+        ///     A concurrency violation is encountered while saving to the database. A concurrency violation occurs when an unexpected number of
+        ///     rows are affected during save. This is usually because the data in the database has been modified since it was loaded into memory.
+        /// </exception>
         public async Task<int> AddRangeAsync(IEnumerable<TModel> models, CancellationToken stoppingToken = default)
         {
-            await this.context.Set<TModel>().AddRangeAsync(models, stoppingToken);
-            return await this.context.SaveChangesAsync(stoppingToken);
+            await this.context.Set<TModel>().AddRangeAsync(models, stoppingToken).ConfigureAwait(false);
+            return await this.context.SaveChangesAsync(stoppingToken).ConfigureAwait(false);
         }
 
         /// <inheritdoc />
-        public bool Exists(TIdentity id) => this.context.Set<TModel>().Any(e => e.Id.CompareTo(id) == 0);
-
-        /// <inheritdoc />
-        public async Task<bool> ExistsAsync(TIdentity id, CancellationToken stoppingToken = default)
-            => await this.context.Set<TModel>().AnyAsync(e => e.Id.CompareTo(id) == 0, stoppingToken).ConfigureAwait(false);
-
-        /// <inheritdoc />
-        public List<TModel> GetAll(params Expression<Func<TModel, object>>[] includes)
-        {
-            var set = this.context.Set<TModel>();
-            var includeSet = GetIncludeSet(includes, set);
-            return includeSet == null ? set.ToList() : includeSet.ToList();
-        }
-
-        /// <inheritdoc />
-        public List<TModel> GetAll(int skip, int take)
-            => this.SkipAndTake(skip, take).ToList();
+        public Task<bool> ExistsAsync(TIdentity id, CancellationToken stoppingToken = default)
+            => this.context.Set<TModel>().AnyAsync(e => e.Id.CompareTo(id) == 0, stoppingToken);
 
         /// <inheritdoc />
         public async Task<List<TModel>> GetAllAsync(CancellationToken stoppingToken = default, params Expression<Func<TModel, object>>[] includes)
@@ -112,21 +94,8 @@ namespace Ustilz.Data
         }
 
         /// <inheritdoc />
-        public async Task<List<TModel>> GetAllAsync(int skip, int take, CancellationToken stoppingToken = default)
-            => await this.SkipAndTake(skip, take).ToListAsync(stoppingToken);
-
-        /// <inheritdoc />
-        public TModel GetDetails(TIdentity id, params Expression<Func<TModel, object>>[] includes)
-        {
-            var set = this.context.Set<TModel>();
-            var includeSet = GetIncludeSet(includes, set);
-            return includeSet == null
-                       ? set.SingleOrDefault(model => model.Id.CompareTo(id) == 0)
-                       : includeSet.SingleOrDefault(model => model.Id.CompareTo(id) == 0);
-        }
-
-        /// <inheritdoc />
-        public TModel GetDetails(TIdentity id) => this.context.Set<TModel>().SingleOrDefault(model => model.Id.CompareTo(id) == 0);
+        public Task<List<TModel>> GetAllAsync(int skip, int take, CancellationToken stoppingToken = default)
+            => this.SkipAndTake(skip, take).ToListAsync(stoppingToken);
 
         /// <inheritdoc />
         public async Task<TModel> GetDetailsAsync(TIdentity id, CancellationToken stoppingToken = default, params Expression<Func<TModel, object>>[] includes)
@@ -134,82 +103,72 @@ namespace Ustilz.Data
             var set = this.context.Set<TModel>();
             var includeSet = GetIncludeSet(includes, set);
             return includeSet == null
-                       ? await set.SingleOrDefaultAsync(model => model.Id.CompareTo(id) == 0, stoppingToken)
-                       : await includeSet.SingleOrDefaultAsync(model => model.Id.CompareTo(id) == 0, stoppingToken);
+                       ? await set.SingleOrDefaultAsync(model => model.Id.CompareTo(id) == 0, stoppingToken).ConfigureAwait(false)
+                       : await includeSet.SingleOrDefaultAsync(model => model.Id.CompareTo(id) == 0, stoppingToken).ConfigureAwait(false);
         }
 
         /// <inheritdoc />
-        public async Task<TModel> GetDetailsAsync(TIdentity id, CancellationToken stoppingToken = default)
-            => await this.context.Set<TModel>().SingleOrDefaultAsync(model => model.Id.CompareTo(id) == 0, stoppingToken);
+        public Task<TModel> GetDetailsAsync(TIdentity id, CancellationToken stoppingToken = default)
+            => this.context.Set<TModel>().SingleOrDefaultAsync(model => model.Id.CompareTo(id) == 0, stoppingToken);
 
         /// <inheritdoc />
-        public int Remove(TModel model)
-        {
-            this.context.Set<TModel>().Remove(model);
-            return this.context.SaveChanges();
-        }
-
-        /// <inheritdoc />
-        public int RemoveAll()
-        {
-            this.context.Set<TModel>().RemoveRange(this.Queryable.ToList());
-            return this.context.SaveChanges();
-        }
-
-        /// <inheritdoc />
+        /// <exception cref="DbUpdateException">An error is encountered while saving to the database.</exception>
+        /// <exception cref="DbUpdateConcurrencyException">
+        ///     A concurrency violation is encountered while saving to the database. A concurrency violation occurs when an unexpected number of
+        ///     rows are affected during save. This is usually because the data in the database has been modified since it was loaded into memory.
+        /// </exception>
         public async Task<int> RemoveAllAsync(CancellationToken stoppingToken = default)
         {
             this.context.Set<TModel>().RemoveRange(await this.Queryable.ToListAsync(stoppingToken).ConfigureAwait(false));
-            return await this.context.SaveChangesAsync(stoppingToken);
+            return await this.context.SaveChangesAsync(stoppingToken).ConfigureAwait(false);
         }
 
         /// <inheritdoc />
-        public async Task<int> RemoveAsync(TModel model, CancellationToken stoppingToken = default)
+        /// <exception cref="DbUpdateException">An error is encountered while saving to the database.</exception>
+        /// <exception cref="DbUpdateConcurrencyException">
+        ///     A concurrency violation is encountered while saving to the database. A concurrency violation occurs when an unexpected number of
+        ///     rows are affected during save. This is usually because the data in the database has been modified since it was loaded into memory.
+        /// </exception>
+        public Task<int> RemoveAsync(TModel model, CancellationToken stoppingToken = default)
         {
             this.context.Set<TModel>().Remove(model);
-            return await this.context.SaveChangesAsync(stoppingToken);
+            return this.context.SaveChangesAsync(stoppingToken);
         }
 
         /// <inheritdoc />
-        public int RemoveRange(IEnumerable<TModel> models)
+        /// <exception cref="DbUpdateException">An error is encountered while saving to the database.</exception>
+        /// <exception cref="DbUpdateConcurrencyException">
+        ///     A concurrency violation is encountered while saving to the database. A concurrency violation occurs when an unexpected number of
+        ///     rows are affected during save. This is usually because the data in the database has been modified since it was loaded into memory.
+        /// </exception>
+        public Task<int> RemoveRangeAsync(IEnumerable<TModel> models, CancellationToken stoppingToken = default)
         {
             this.context.Set<TModel>().RemoveRange(models);
-            return this.context.SaveChanges();
+            return this.context.SaveChangesAsync(stoppingToken);
         }
 
         /// <inheritdoc />
-        public async Task<int> RemoveRangeAsync(IEnumerable<TModel> models, CancellationToken stoppingToken = default)
-        {
-            this.context.Set<TModel>().RemoveRange(models);
-            return await this.context.SaveChangesAsync(stoppingToken).ConfigureAwait(false);
-        }
-
-        /// <inheritdoc />
-        public int Update(TModel model)
+        /// <exception cref="DbUpdateException">An error is encountered while saving to the database.</exception>
+        /// <exception cref="DbUpdateConcurrencyException">
+        ///     A concurrency violation is encountered while saving to the database. A concurrency violation occurs when an unexpected number of
+        ///     rows are affected during save. This is usually because the data in the database has been modified since it was loaded into memory.
+        /// </exception>
+        public Task<int> UpdateAsync(TModel model, CancellationToken stoppingToken = default)
         {
             this.context.Set<TModel>().Update(model);
-            return this.context.SaveChanges();
+            return this.context.SaveChangesAsync(stoppingToken);
         }
 
         /// <inheritdoc />
-        public async Task<int> UpdateAsync(TModel model, CancellationToken stoppingToken = default)
-        {
-            this.context.Set<TModel>().Update(model);
-            return await this.context.SaveChangesAsync(stoppingToken);
-        }
-
-        /// <inheritdoc />
-        public int UpdateRange(IEnumerable<TModel> models)
+        /// <exception cref="DbUpdateException">An error is encountered while saving to the database.</exception>
+        /// <exception cref="DbUpdateConcurrencyException">
+        ///     A concurrency violation is encountered while saving to the database. A concurrency violation occurs when an unexpected number of
+        ///     rows are affected during save. This is usually because the data in the database has been modified since it was loaded into memory.
+        /// </exception>
+        public Task<int> UpdateRangeAsync(IEnumerable<TModel> models, CancellationToken stoppingToken = default)
         {
             this.context.Set<TModel>().UpdateRange(models);
-            return this.context.SaveChanges();
-        }
-
-        /// <inheritdoc />
-        public async Task<int> UpdateRangeAsync(IEnumerable<TModel> models, CancellationToken stoppingToken = default)
-        {
-            this.context.Set<TModel>().UpdateRange(models);
-            return await this.context.SaveChangesAsync(stoppingToken).ConfigureAwait(false);
+            return this.context.SaveChangesAsync(stoppingToken);
         }
 
         #endregion
--- src/Ustilz.Data/Interfaces/IBaseDAL.cs
+++ src/Ustilz.Data/Interfaces/IBaseDAL.cs
@@ -9,11 +9,14 @@ namespace Ustilz.Data.Interfaces
     using System.Threading;
     using System.Threading.Tasks;
 
+    using JetBrains.Annotations;
+
     #endregion
 
     /// <summary>Interface de définition des méthodes de base des DAL.</summary>
     /// <typeparam name="TModel">Modèle de la DAL.</typeparam>
     /// <typeparam name="TIdentity">Type de la clé primaire.</typeparam>
+    [PublicAPI]
     public interface IBaseDAL<TModel, in TIdentity>
         where TModel : class, IDto<TIdentity>
         where TIdentity : IComparable<TIdentity>
@@ -29,19 +32,9 @@ namespace Ustilz.Data.Interfaces
 
         /// <summary>The create.</summary>
         /// <param name="model">The model.</param>
-        /// <returns>The <see cref="Task" />.</returns>
-        int Add(TModel model);
-
-        /// <summary>The create.</summary>
-        /// <param name="model">The model.</param>
         /// <param name="stoppingToken">Token d'arret de la tache.</param>
         /// <returns>The <see cref="Task" />.</returns>
         Task<int> AddAsync(TModel model, CancellationToken stoppingToken = default);
-
-        /// <summary>Méthode d'ajout d'une liste d'éléments.</summary>
-        /// <param name="models">La liste d'éléments à ajouter.</param>
-        /// <returns>Retourne le nombre de ligne impactées.</returns>
-        int AddRange(IEnumerable<TModel> models);
 
         /// <summary>Méthode d'ajout d'une liste d'éléments.</summary>
         /// <param name="models">La liste d'éléments à ajouter.</param>
@@ -51,25 +44,9 @@ namespace Ustilz.Data.Interfaces
 
         /// <summary>The exists.</summary>
         /// <param name="id">The id.</param>
-        /// <returns>The <see cref="bool" />.</returns>
-        bool Exists(TIdentity id);
-
-        /// <summary>The exists.</summary>
-        /// <param name="id">The id.</param>
         /// <param name="stoppingToken">Token d'arret de la tache.</param>
         /// <returns>The <see cref="bool" />.</returns>
         Task<bool> ExistsAsync(TIdentity id, CancellationToken stoppingToken = default);
-
-        /// <summary>The get all.</summary>
-        /// <param name="includes">The includes.</param>
-        /// <returns>The <see cref="Task" />.</returns>
-        List<TModel> GetAll(params Expression<Func<TModel, object>>[] includes);
-
-        /// <summary>The get all with pagination.</summary>
-        /// <param name="skip">The skip.</param>
-        /// <param name="take">The take.</param>
-        /// <returns>The <see cref="Task" />.</returns>
-        List<TModel> GetAll(int skip, int take);
 
         /// <summary>The get all.</summary>
         /// <param name="stoppingToken">Token d'arret de la tache.</param>
@@ -86,17 +63,6 @@ namespace Ustilz.Data.Interfaces
 
         /// <summary>The get details.</summary>
         /// <param name="id">The id.</param>
-        /// <param name="includes">The includes.</param>
-        /// <returns>The <see cref="Task" />.</returns>
-        TModel GetDetails(TIdentity id, params Expression<Func<TModel, object>>[] includes);
-
-        /// <summary>The get details.</summary>
-        /// <param name="id">The id.</param>
-        /// <returns>The <see cref="Task" />.</returns>
-        TModel GetDetails(TIdentity id);
-
-        /// <summary>The get details.</summary>
-        /// <param name="id">The id.</param>
         /// <param name="stoppingToken">Token d'arret de la tache.</param>
         /// <param name="includes">The includes.</param>
         /// <returns>The <see cref="Task" />.</returns>
@@ -107,15 +73,6 @@ namespace Ustilz.Data.Interfaces
         /// <param name="stoppingToken">Token d'arret de la tache.</param>
         /// <returns>The <see cref="Task" />.</returns>
         Task<TModel> GetDetailsAsync(TIdentity id, CancellationToken stoppingToken = default);
-
-        /// <summary>The remove.</summary>
-        /// <param name="model">The model.</param>
-        /// <returns>The <see cref="Task" />.</returns>
-        int Remove(TModel model);
-
-        /// <summary>Méthode suppression des données de la table.</summary>
-        /// <returns>Retourne le nombre de ligné impactées.</returns>
-        int RemoveAll();
 
         /// <summary>Méthode suppression des données de la table.</summary>
         /// <param name="stoppingToken">Token d'arret de la tache.</param>
@@ -130,30 +87,15 @@ namespace Ustilz.Data.Interfaces
 
         /// <summary>Méthode suppression des données de la table.</summary>
         /// <param name="models">Liste des modèles à supprimer.</param>
-        /// <returns>Retourne le nombre de ligné impactées.</returns>
-        int RemoveRange(IEnumerable<TModel> models);
-
-        /// <summary>Méthode suppression des données de la table.</summary>
-        /// <param name="models">Liste des modèles à supprimer.</param>
         /// <param name="stoppingToken">Token d'arret de la tache.</param>
         /// <returns>Retourne le nombre de ligné impactées.</returns>
         Task<int> RemoveRangeAsync(IEnumerable<TModel> models, CancellationToken stoppingToken = default);
 
         /// <summary>The update.</summary>
         /// <param name="model">The model.</param>
-        /// <returns>The <see cref="Task" />.</returns>
-        int Update(TModel model);
-
-        /// <summary>The update.</summary>
-        /// <param name="model">The model.</param>
         /// <param name="stoppingToken">Token d'arret de la tache.</param>
         /// <returns>The <see cref="Task" />.</returns>
         Task<int> UpdateAsync(TModel model, CancellationToken stoppingToken = default);
-
-        /// <summary>The update range.</summary>
-        /// <param name="models">La liste des modèles à mettre à jour.</param>
-        /// <returns>The <see cref="Task" />.</returns>
-        int UpdateRange(IEnumerable<TModel> models);
 
         /// <summary>The update range.</summary>
         /// <param name="models">La liste des modèles à mettre à jour.</param>
